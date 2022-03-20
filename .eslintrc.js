d9b162bffe23354f217d5ee09b6c79186cddecdc
Raymond Kalumba Joseph <raymondkalumba360@gmail.com>
2022-03-20 07:27:24 +0300

    new status files

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@paralect/eslint-config', 'plugin:react-hooks/recommended'],
-  rules: {
-    'object-curly-newline': 'off',
-    'arrow-parens': 'always',
-  },
-};
