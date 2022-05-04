cf3d4eca8177bac357b41f48cc9952ff2e2241ea
raj360 <raymondkalumba360@gmail.com>
2022-05-04 09:44:23 +0300

    remove drawer library

--- src/navigation/navigation.jsx
+++ src/navigation/navigation.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import 'react-native-gesture-handler';
 import { navigationRef, routeNameRef } from 'helpers/rootNavigation';
 
